3eef4c100eacd924e8477cd42edb2e1670c5b969
asxhsen <rkalbertsonsingh@gmail.com>
2025-06-01 14:12:37 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function startSite() {
-  document.getElementById("startScreen").style.display = "none";
-  document.getElementById("mainContent").classList.remove("hidden");
-
-  const audio = document.getElementById("birthdayMusic");
-  audio.play();
-}
