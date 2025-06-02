7518f03fbfc1138bae6e12cfb724d52ebfec4d1d
asxhsen <rkalbertsonsingh@gmail.com>
2025-06-02 19:26:34 +0530

    Create photos.js

---
+++ photos.js
@@ -0,0 +1,4 @@
+const surbalaPhotos = [
+    { url: "https://raw.githubusercontent.com/asxhsen/Surbala_birthday_site/main/photos/surbala_funny.jpg", caption: "gadha dune ac d🔥🔥🩷🩷❤‍🔥❤‍🔥🔥🔥" },
+    { url: "https://raw.githubusercontent.com/asxhsen/Surbala_birthday_site/main/photos/surbala_pretty.jpg", caption: "Our Stunning gng! ✨🩷" }
+];
